9414d98db9f363b4e7b6b7d0f6bd5fcd9ab0ff8e
Alexey Minchakov <lexaaim@gmail.com>
2020-03-14 20:49:39 +0300

    improve drawing even more

--- AlgoDrawer.py
+++ AlgoDrawer.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 def drawAlgoInfo(info, market, share):
     minDay, maxDay = min(info.axisDT).date(), max(info.axisDT).date()
 
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
-    fig.suptitle("{0}".format(market), fontsize=16)
+    fig, axes = plt.subplots(3, 1, sharex=True, gridspec_kw={ 'height_ratios': [5, 2, 2] })
+    ax1, ax2, ax3 = axes
+    fig.suptitle('{0} market'.format(market), fontsize=16)
     fig.tight_layout()
-    plt.subplots_adjust(hspace=0.1, left=0.07)
+    plt.subplots_adjust(hspace=0.09, left=0.07, right=0.94, bottom=0.04, top=0.935)
 
     ax1.plot(info.axisDT, info.fnPrice,
         color="#2980b9",
@@ -23,9 +24,12 @@ def drawAlgoInfo(info, market, share):
     ax1.axis([minDay, maxDay, 0, max(info.fnPrice) + 5])
     ax1.set_ylabel("USD")
     ax1.legend()
+    ax1.tick_params(top=True, right=True, left=True, bottom=True, labeltop=True, labelright=True)
+    ax1.grid(True, which="major", axis="x", linestyle="--")
 
     ax2.fill_between(info.axisDT, 0, info.fnCount,
-        facecolor="#9b59b6",
+        facecolor="#fda7df",
+        edgecolor="#8e44ad",
         joinstyle="bevel",
         capstyle="butt",
         step="mid",
@@ -33,12 +37,18 @@ def drawAlgoInfo(info, market, share):
     ax2.axis([minDay, maxDay, min(info.fnCount), max(info.fnCount) + 5])
     ax2.set_ylabel("Amount")
     ax2.legend()
+    ax2.tick_params(top=True, right=True, left=True, bottom=True, labeltop=True, labelright=True)
+    ax2.grid(True, which="major", axis="both", linestyle="--")
 
     ax3.plot(info.axisDT, info.fnCapital,
         color="#c0392b",
-        label="Capital")
-    ax3.axis([minDay, maxDay, 0, max(info.fnCapital) + 10])
+        label="capital")
+    ax3.axis([minDay, maxDay, 0, max(info.fnCapital) + 100])
     ax3.set_ylabel("USD")
     ax3.legend()
+    ax3.tick_params(top=True, right=True, left=True, bottom=True, labeltop=True, labelright=True)
+    ax3.grid(True, which="major", axis="both", linestyle="--")
+
+    fig.align_labels()
 
     plt.show()
