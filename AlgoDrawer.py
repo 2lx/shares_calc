9deb517aaed7e437d2cc358cde3eddb8fa702525
Alexey Minchakov <lexaaim@gmail.com>
2020-03-14 18:34:57 +0300

    improve drawing

--- AlgoDrawer.py
+++ AlgoDrawer.py
@@ -6,21 +6,39 @@ def drawAlgoInfo(info, market, share):
     minDay, maxDay = min(info.axisDT).date(), max(info.axisDT).date()
 
     fig, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
-    fig.suptitle("{0}, {1}".format(market, share), fontsize=16)
+    fig.suptitle("{0}".format(market), fontsize=16)
     fig.tight_layout()
     plt.subplots_adjust(hspace=0.1, left=0.07)
 
-    ax1.plot(info.axisDT, info.fnPrice, color="#2980b9")
-    ax1.fill_between(info.axisDT, 0, info.fnSoldPrice, facecolor="#bdc3c7", edgecolor="#7f8c8d", interpolate=False)
+    ax1.plot(info.axisDT, info.fnPrice,
+        color="#2980b9",
+        label="AMD shares price")
+    ax1.fill_between(info.axisDT, 0, info.fnSoldPrice,
+        facecolor="#bdc3c7",
+        edgecolor="#7f8c8d",
+        joinstyle="bevel",
+        capstyle="butt",
+        step="mid",
+        label="exit price")
     ax1.axis([minDay, maxDay, 0, max(info.fnPrice) + 5])
-    ax1.set_ylabel("Prices")
+    ax1.set_ylabel("USD")
+    ax1.legend()
 
-    ax2.fill_between(info.axisDT, 0, info.fnCount, facecolor="#9b59b6", interpolate=False, joinstyle="round")
+    ax2.fill_between(info.axisDT, 0, info.fnCount,
+        facecolor="#9b59b6",
+        joinstyle="bevel",
+        capstyle="butt",
+        step="mid",
+        label="acquired AMD shares")
     ax2.axis([minDay, maxDay, min(info.fnCount), max(info.fnCount) + 5])
-    ax2.set_ylabel("Share count")
+    ax2.set_ylabel("Amount")
+    ax2.legend()
 
-    ax3.plot(info.axisDT, info.fnCapital, color="#c0392b")
+    ax3.plot(info.axisDT, info.fnCapital,
+        color="#c0392b",
+        label="Capital")
     ax3.axis([minDay, maxDay, 0, max(info.fnCapital) + 10])
-    ax3.set_ylabel("Capital")
+    ax3.set_ylabel("USD")
+    ax3.legend()
 
     plt.show()
